eb577bf0cd9997fd54cdfdf7c706100bffbf065b
Tom OB <tobrien999@gmail.com>
2011-02-22 10:37:49 -0700

    changes to .gitignore

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,6 @@
 class Asset < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :photo  #, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
                     # :storage => :s3,
                     #                     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
                     #                     :path => ":id/:style/:filename"
